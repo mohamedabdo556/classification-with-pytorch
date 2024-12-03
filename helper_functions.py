63a02cb24988032c02156f1a571a7f483c5cd651
mohamed abdo <137591248+mohamedabdo556@users.noreply.github.com>
2024-12-03 16:56:14 +0200

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,8 +1,4 @@
-"""
-A series of helper functions used throughout the course.
 
-If a function gets defined once and could be used over and over, it'll go in here.
-"""
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
